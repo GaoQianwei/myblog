471e9eb769c9f85abc888b3a73109df0bae22259
Gnoch <864807289@qq.com>
2020-07-01 16:42:51 +0800

    change

--- src/main/java/noobteam/myblog/mapper/Gnoch/VisitsetMapper.java
+++ src/main/java/noobteam/myblog/mapper/Gnoch/VisitsetMapper.java
@@ -2,6 +2,7 @@ package noobteam.myblog.mapper.Gnoch;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.Map;
@@ -10,4 +11,15 @@ import java.util.Map;
 public interface VisitsetMapper {
     @Update("update visitset "+" set prmsset=#{u.prmsset} "+" where no=#{u.no}")
     public int PermissionSet(@Param("u") Map<String, String>map);
+
+    @Select("select prmsset from visitset where no=#{u.no}")
+    public String GetPrmsset(@Param("u") Map<String ,String>map);
+
+    @Update("update visitset "+" set dectype=#{u.dectype} "+" where no=#{u.no}")
+    public int DecSet(@Param("u") Map<String ,String>map);
+
+    @Select("select dectype from visitset where no=#{u.no}")
+    public String GetDec(@Param("u") Map<String ,String>map);
+
+
 }
