db5300dea15368ce78d650b2c477377411e083d5
zns <liu1131451057@gmail.com>
2020-07-01 11:49:19 +0800

    add cookie and pwdReset module

--- src/main/java/noobteam/myblog/controller/Zns/SignController.java
+++ src/main/java/noobteam/myblog/controller/Zns/SignController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,20 +27,24 @@ public class SignController {
         return list;
     }
     **/
-    public boolean findall(@RequestParam Map<String,String> js){
+    public Map<String,String> findall(@RequestParam Map<String,String> js){
         boolean flag = false;
         List<Map<String,Object>> list = signService.findall();
+        Map<String,String> updatejs = new HashMap<>();
         try {
             for(Map<String,Object> map : list){
                 if(map.get("name").equals(js.get("name")) && map.get("code").equals(js.get("pwd"))){
-                    Map<String,String> updatejs = updateDate(map.get("no").toString());
+                    updatejs = updateDate(map.get("no").toString());
                     flag = signService.updateUsr_stus(updatejs);
+                    if(flag)
+                        updatejs.put("flag", "true");
+                    else updatejs.put("flag", "false");
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return flag;
+        return updatejs;
     }
 
     public Map<String,String> updateDate(String no){
@@ -51,6 +57,15 @@ public class SignController {
         return map;
     }
 
+    @RequestMapping("/setCookies_no")
+    public String setCookies(HttpServletResponse response, String no){
+        Cookie cookie = new Cookie("usr_no", no);
+        response.addCookie(cookie);
+        System.out.println("Cookies添加成功");
+        return "Cookies添加成功";
+    }
+
+
     @RequestMapping("/signup")
     public boolean insert(@RequestParam Map<String,String> js){
         String no = MathUtils.makeUpNewData(Thread.currentThread().hashCode()+"", 3)+ MathUtils.randomDigitNumber(7);
@@ -58,4 +73,7 @@ public class SignController {
         boolean flag = signService.insert(js);
         return flag;
     }
+
+
+
 }
