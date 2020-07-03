fcbff3f6c82de5dec9920891ac0d91d3d5ee7afe
Your Name <you@example.com>
2020-07-03 14:52:48 +0800

    zz

---
+++ src/test/java/noobteam/myblog/mapper/Zhang/FriendlistMapperTest.java
@@ -0,0 +1,16 @@
+package noobteam.myblog.mapper.Zhang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FriendlistMapperTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
